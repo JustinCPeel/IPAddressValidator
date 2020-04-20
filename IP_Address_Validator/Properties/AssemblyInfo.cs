7c92bcbdbb1ec8327648ca23fd8940dc0e973a4e
Justin Peel <justinpeel25@gmail.com>
2020-04-20 14:32:54 +0200

    Initial Commit

---
+++ IP_Address_Validator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IP_Address_Validator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IP_Address_Validator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94e77db6-15ed-4997-add9-f86d999c6252")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IP_Address_Validator/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace IP_Address_Validator
+{
+    [TestFixture]
+    public class IP_Address_Validator
+    {
+        [Test]
+        public void IPv4_Address1ShouldReturnTrue()
+        {
+            //arrange
+            var ipAddress= "1.1.1.1";
+            //act
+            var expected = true;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address2ShouldReturnTrue()
+        {
+            //arrange
+            var ipAddress = "192.168.1.1";
+            //act
+            var expected = true;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address3ShouldReturnTrue()
+        {
+            //arrange
+            var ipAddress = "10.0.0.1";
+            //act
+            var expected = true;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address4ShouldReturnTrue()
+        {
+            //arrange
+            var ipAddress = "127.0.0.1";
+            //act
+            var expected = true;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address5ShouldReturnFalse()
+        {
+            //arrange
+            var ipAddress = "0.0.0.0";
+            //act
+            var expected = false;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address6ShouldReturnFalse ()
+        {
+            //arrange
+            var ipAddress = "255.255.255.255";
+            //act
+            var expected = false;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address7ShouldReturnFalse()
+        {
+            //arrange
+            var ipAddress = "1.1.1.0";
+            //act
+            var expected = false;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IPv4_Address8ShouldReturnFalse()
+        {
+            //arrange
+            var ipAddress = "10.0.1";
+            //act
+            var expected = false;
+            var actual = ValidateIpv4Address(ipAddress);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private bool ValidateIpv4Address(string ipAddress)
+        {
+            
+            string lastThreeDigits = ipAddress.Substring(ipAddress.Length - 3);
+            char lastDigitIp = Convert.ToChar(lastThreeDigits[2]);
+
+            if (lastThreeDigits.Contains("255") || lastDigitIp == '0'|| ipAddress.Split('.').Length < 4 )
+                return false;
+            else
+                return true;
+        }
+    }
+}
